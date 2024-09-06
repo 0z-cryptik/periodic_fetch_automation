aad636b0ae2091e39966abe235a461b441851c5d
Eniitan <enitanolawale0@gmail.com>
2024-09-06 17:36:30 +0100

    added dotenv

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def send_request_to_url(url):
     try:
-        response = requests.get(url, timeout=5)
-        return response.status_code, response.json()
+        response = requests.get(url, timeout=120)
+        return response.status_code
     except requests.ConnectionError:
         return None, "No internet connection or URL unreachable."
     except requests.RequestException as e:
@@ -11,15 +16,18 @@ def send_request_to_url(url):
 
 
 def main():
-    url = "https://projectplanner-backend.onrender.com/periodicFetch"
-    status_code, content = send_request_to_url(url)
+    url = os.getenv("URL")
 
-    if status_code:
+    if not url:
+        print("URL is not set in the environment file.")
+        return
+
+    status_code = send_request_to_url(url)
+
+    if status_code == 200:
         print(f"Request successful. Status code: {status_code}")
-        print("Response content:")
-        print(content)
     else:
-        print(f"Failed to send request. Error: {content}")
+        print("Error sending request")
 
 
 if __name__ == "__main__":
